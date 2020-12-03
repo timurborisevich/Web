9f5174c134a1f62dea4527cc40c859e16376ca9a
timur.borisevich <timurborisevich@mail.ru>
2020-12-03 17:15:23 +0300

    1.9.11

--- etc/hello.py
+++ etc/hello.py
@@ -1,4 +1,10 @@
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+# gunicorn configuration file for "hello" python application
+
+pythonpath = '/home/box/web/'  # путь до приложения
+bind = "0.0.0.0:8080"
+workers = 4
+
+# def app(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+#                   encoding="utf8")]

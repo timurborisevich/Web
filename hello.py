17aa0f0e5d6d410d54d19a1269377ee3000fff67
timur.borisevich <timurborisevich@mail.ru>
2020-12-03 17:49:46 +0300

    1.9.11

--- hello.py
+++ hello.py
@@ -2,3 +2,5 @@ def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
                   encoding="utf8")]
+
+bind = "0.0.0.0:8080"

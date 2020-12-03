0dfbc34b00a37a84eced762316a034538088ae32
timur.borisevich <timurborisevich@mail.ru>
2020-12-03 17:05:09 +0300

    1.9.11

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo gunicorn -w 4 /home/box/web/etc/hello:app
-/home/box/web/etc sudo gunicorn - b 0.0.0.0:8080 hello:app
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app
+#cd /home/box/web/etc sudo gunicorn - b 0.0.0.0:8080 hello:app
